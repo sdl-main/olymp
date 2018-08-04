deda0908dab3e2d3714e19f20566ca760270afaa
sdl <s.lebkov@gmail.com>
2018-08-05 00:01:55 +1100

    added template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define INF 2000000000
+#define pb push_back
+#define pii pair<int,int>
+
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    return 0;
+}
